a7c5563417589be83897cedee6438a7f88960c8d
Chris Wilson <usediv@gmail.com>
2019-10-05 20:18:56 -0400

    Welcome string copy update

--- do/strings.py
+++ do/strings.py
@@ -1,5 +1,5 @@
 def welcome():
-    return "Welcome to Do! Do is an SMS app that can help you make or break habits by checking in every day to see how you're doing and tracking your progress."
+    return "Welcome to Do! Do is an SMS app that can help you make or break habits by checking in with you and tracking your progress daily"
 
 def down():
     return "Sorry, Do isn't available right now. Please try again later"
