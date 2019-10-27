38b7a8694cde45db137dae5de3c5184e3efa2dfe
Chris Wilson <usediv@gmail.com>
2019-10-27 08:38:15 -0400

    Consolidated achievement response tests

--- test_test.py
+++ test_test.py
@@ -6,45 +6,15 @@ from do.strings import achievement_confirmation_text
 
 class Test(unittest.TestCase):
 
-    def test_achievement_confirmation_nil_all(self):
-        streak=0
-        count=0
-        start_date = date(2019,7,1)
-        result = achievement_confirmation_text(count,streak,start_date)
-        self.assertEqual(result,"Nevermind! Today's a new day and another chance to achieve your goal!")
-
-    def test_achievement_confirmation_on_the_board(self):
-        streak=1
-        count=1
-        start_date = date(2019,7,1)
-        result = achievement_confirmation_text(count,streak,start_date)
-        self.assertEqual(result,"Great job! Congrats on achieving your first daily goal!")
-
-    def test_achievement_confirmation_streaking(self):
-        streak=2
-        count=1
-        start_date = date(2019,7,1)
-        result = achievement_confirmation_text(count,streak,start_date)
-        self.assertEqual(result,f"Nice work! That's {streak} days in a row and {count} days that you've achieved your goal in total. Keep up the good work!")
-
-    def test_achievement_confirmation_fail(self):
-        streak=0
-        count=2
+    def test_achievement_confirmation_text(self):
         start_date = date(2019,7,1)
         delta = today-start_date
         days = delta.days
-        result = achievement_confirmation_text(count,streak,start_date)
-        self.assertEqual(result,f"Nevermind! Today's a new day and another chance to achieve your goal! You've achieved your daily goal {count} times in {days} days.")
-
-    def test_achievement_back_on_the_horse(self):
-        streak=1
-        count=2
-        start_date = date(2019,7,1)
-        delta = today-start_date
-        days = delta.days
-        result = achievement_confirmation_text(count,streak,start_date)
-        self.assertEqual(result,f"Great job! Congrats on getting back on the horse! You've achieved your daily goal {count} times in {days} days.")
-
+        self.assertEqual(achievement_confirmation_text(0,0,start_date),"Nevermind! Today's a new day and another chance to achieve your goal!")
+        self.assertEqual(achievement_confirmation_text(1,1,start_date),"Great job! Congrats on achieving your first daily goal!")
+        self.assertEqual(achievement_confirmation_text(3,2,start_date),"Nice work! That's 2 days in a row and 3 days that you've achieved your goal in total. Keep up the good work!")
+        self.assertEqual(achievement_confirmation_text(2,0,start_date),f"Nevermind! Today's a new day and another chance to achieve your goal! You've achieved your daily goal 2 times in {days} days.")
+        self.assertEqual(achievement_confirmation_text(2,1,start_date),f"Great job! Congrats on getting back on the horse! You've achieved your daily goal 2 times in {days} days.")
 
 
 if __name__ == '__main__':
